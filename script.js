3cb4edab3da6784e0bbcd72ed99b77887025a7f4
plector <kenanpoole@gmail.com>
2021-11-06 18:24:46 -0400

    Keyboard WORK progress let's go!

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.onkeydown = KeyCheck;   
 document.onkeyup = LogKey;
 
+let root = document.documentElement;
+
 var cPressed = false;
+var csharpPressed = false;
 
 function KeyCheck(event){
     event.preventDefault();
@@ -14,11 +17,20 @@ function KeyCheck(event){
         case 90:
             if(!cPressed){
                 console.log("First Press");
-                document.getElementsByClassName("container").gridTemplateColumns = "1fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr";
+                //document.getElementsByClassName("container").gridTemplateColumns = "1fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr";
+                root.style.setProperty('--c', 1 + "fr");
+                
             }
             console.log(document.getElementsByClassName("container"));
             //document.body.style.backgroundColor = "rgb(255,0,0)";
             cPressed = true;
+            break;
+
+        case 83:
+            if(!csharpPressed){
+                root.style.setProperty("--csharp", 1 + "fr");
+            }
+            csharpPressed = true;
             break;
 
         case 66:
@@ -34,13 +46,14 @@ function LogKey(event){
     console.log(KeyID);
 
     switch(KeyID){
-        case 65:
-            document.body.style.backgroundColor = "white";
-            console.log(document.body.style.backgroundColor);
+        case 90:
+            root.style.setProperty("--c", 0 + "fr");
+            cPressed = false;
             break;
 
-        case 66:
-            document.body.style.backgroundColor = "white";
+        case 83:
+            root.style.setProperty("--csharp", 0 + "fr");
+            csharpPressed = false;
             break;
     }
 }
