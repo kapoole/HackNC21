4020c1209bac44382dee81254be820aeae2f8633
plector <kenanpoole@gmail.com>
2021-11-06 17:41:30 -0400

    CSS grid mostly functioning

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 document.onkeydown = KeyCheck;   
 document.onkeyup = LogKey;
 
+var cPressed = false;
+
 function KeyCheck(event){
     event.preventDefault();
     var KeyID = event.keyCode;
     console.log(KeyID);
 
+    console.log(document.getElementsByClassName("container"));
+
     switch(KeyID){
-        case 65:
-            document.body.style.backgroundColor = "rgb(255,0,0)";
+        case 90:
+            if(!cPressed){
+                console.log("First Press");
+                document.getElementsByClassName("container").gridTemplateColumns = "1fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr 0fr";
+            }
+            console.log(document.getElementsByClassName("container"));
+            //document.body.style.backgroundColor = "rgb(255,0,0)";
+            cPressed = true;
             break;
 
         case 66:
