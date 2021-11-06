6e5b503ac952add71ae560859b40e5873c961994
plector <kenanpoole@gmail.com>
2021-11-06 16:00:41 -0400

    Pass to Jeremy

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 document.onkeydown = KeyCheck;   
+document.onkeyup = LogKey;
 
-var redhex = 0;
-var bluehex = 0;
-var greenhex = 0;
-var keysPressed = 0;
+var redhex = 255;
+var greenhex = 255;
+var bluehex = 255;
+var keysPressed = 1;
 
-function KeyCheck(event)
-{
+function KeyCheck(event){
     var KeyID = event.keyCode;
     console.log(KeyID);
 
-    switch(KeyID)
-    {
+    switch(KeyID){
         case 65:
             //HTMLElement.stye="background-color: red";
             keysPressed++;
-            redhex = redhex + 255;
+            redhex = redhex - (redhex + 255)/keysPressed;
+            greenhex = greenhex - 255;
+            bluehex = bluehex - 255;
             console.log(redhex);
             document.body.style.backgroundColor = "rgb(" +redhex+", " + greenhex + ", " + bluehex + ")";
             console.log(document.body.style.backgroundColor);
@@ -29,4 +30,19 @@ function KeyCheck(event)
         break;
         
      }
+}
+
+function LogKey(event){
+    var KeyID = event.keyCode;
+    console.log(KeyID);
+
+    switch(KeyID){
+
+        case 65:
+            keysPressed--;
+            redhex = redhex - 255;
+            document.body.style.backgroundColor = "rgb(" +redhex+", " + greenhex + ", " + bluehex + ")";
+            console.log(document.body.style.backgroundColor);
+
+    }
 }
