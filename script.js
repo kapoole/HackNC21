585faa5a6c0480dbe2f64005c8658ed6808cc27c
Jeremy Chen <69742661+ziang1@users.noreply.github.com>
2021-11-06 16:19:34 -0400

    set rgb instead of additive

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-document.onkeydown = KeyCheck;   
+document.onkeypress = KeyCheck;   
 document.onkeyup = LogKey;
-
-var redhex = 255;
-var greenhex = 255;
-var bluehex = 255;
-var keysPressed = 1;
 
 function KeyCheck(event){
     var KeyID = event.keyCode;
@@ -12,22 +7,13 @@ function KeyCheck(event){
 
     switch(KeyID){
         case 65:
-            //HTMLElement.stye="background-color: red";
-            keysPressed++;
-            redhex = redhex - (redhex + 255)/keysPressed;
-            greenhex = greenhex - 255;
-            bluehex = bluehex - 255;
-            console.log(redhex);
-            document.body.style.backgroundColor = "rgb(" +redhex+", " + greenhex + ", " + bluehex + ")";
-            console.log(document.body.style.backgroundColor);
-        break;
+            document.body.style.backgroundColor = "rgb(255,0,0)";
+            break;
 
         case 66:
-            keysPressed++;
-            bluehex = bluehex + 255;
-            document.body.style.backgroundColor = "rgb(" +redhex+", " + greenhex + ", " + bluehex + ")";
+            document.body.style.backgroundColor = "rgb(0,0,255)";
             console.log(document.body.style.backgroundColor);
-        break;
+            break;
         
      }
 }
@@ -37,12 +23,13 @@ function LogKey(event){
     console.log(KeyID);
 
     switch(KeyID){
-
         case 65:
-            keysPressed--;
-            redhex = redhex - 255;
-            document.body.style.backgroundColor = "rgb(" +redhex+", " + greenhex + ", " + bluehex + ")";
+            document.body.style.backgroundColor = "white";
             console.log(document.body.style.backgroundColor);
+            break;
 
+        case 66:
+            document.body.style.backgroundColor = "white";
+            break;
     }
 }
