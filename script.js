c716d7077574db3697d52b0a35e6f9e9b7b34d51
plector <kenanpoole@gmail.com>
2021-11-06 22:26:23 -0400

    added notation

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let root = document.documentElement;
 
 // keyCode for every key being used, in order of notes C, C#, ...
 var keyCodes = [90, 83, 88, 68, 67, 86, 71, 66, 72, 78, 74, 77];
+var notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+var notesID = ["cNote", "csharpNote", "dNote", "dsharpNote", "eNote", "fNote", "fsharpNote", "gNote", "gsharpNote", "aNote", "asharpNote", "bNote"];
 // boolean array
 var keyIsPressed = Array(keyCodes.length).fill(false);
 var cssProperties = ['--c', '--csharp', '--d', '--dsharp', '--e', '--f', '--fsharp', '--g', '--gsharp', '--a', '--asharp', '--b'];
@@ -20,5 +22,10 @@ function ToggleKey(event) {
     if (k == -1 || keyIsPressed[k] == isOn) { return; }
     keyIsPressed[k] = isOn;
     root.style.setProperty(cssProperties[k], (isOn ? 1 : 0) + 'fr');
+    if(isOn){
+        document.getElementById(notesID[k]).innerHTML = notes[k];
+    }else{
+        document.getElementById(notesID[k]).innerHTML = "";
+    }
     console.log(cssProperties[k] + ' ' + event.keyCode + ' ' + event.type);
 }
